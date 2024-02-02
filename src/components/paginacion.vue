c80a4c3a538282d6d81daba0a2824a7cb6f598eb
MORGANPOSADA <20213tn035@utez.edu.mx>
2024-02-02 16:59:52 -0600

    third

--- src/components/paginacion.vue
+++ src/components/paginacion.vue
@@ -1,13 +1,43 @@
 <template>
   <div>
      paginación
+     <b-breadcrumb :items="items"></b-breadcrumb>
+        
   </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            items: [
+                {
+                    text: "Inicio",
+                    href: "#",
+                    to: "inicio",
+                },
+                {
+                    text: "Sitio Principal",
+                    href: "#",
+                    to:"sitio-principal"
+                },
+                {
+                    text: "formulario",
+                    href: "#",
+                    to:"formulario"
+                },
+                {
+                    text: "paginacion",
+                    href: "#",
+                   
+                },
+            ],
+        };
+    },
 
-}
+    
+
+};
 </script>
 
 <style>
--- src/components/sitio_principal.vue
+++ src/components/sitio_principal.vue
@@ -3,7 +3,14 @@
         sitio principal 
         <b-breadcrumb :items="items"></b-breadcrumb>
         <b-link :to="{ name: 'formulario' }"> Next</b-link>
+
+        <b-col md="6" class="mb-3">
+      <p>BIENVENIDO!! </p>
+      <b-icon icon="star-fill" animation="fade" font-scale="4"></b-icon>
+    </b-col>
     </div>
+
+    
 </template>
 
 <script>
